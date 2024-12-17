48318e505621518202e7075df1fd282bcbd5bde3
Connor Sanders <connor@elastiflow.com>
2024-12-17 16:05:27 -0600

    * lint fix

--- datastreams/enums.go
+++ datastreams/enums.go
@@ -8,23 +8,3 @@ const (
 	fanIn
 	broadcast
 )
-
-type segment int
-
-const (
-	segmentFilter segment = iota
-	segmentProcess
-	segmentMap
-)
-
-func (s segment) String() string {
-	switch s {
-	case segmentFilter:
-		return "filter"
-	case segmentProcess:
-		return "process"
-	case segmentMap:
-		return "map"
-	}
-	return ""
-}
