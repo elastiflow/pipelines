b04b7efa36164e9a8092fd6d6218692f51262979
Reggie Ross <reggieross07@gmail.com>
2025-04-13 16:32:44 -0500

    chore: Adding keyed datastreams

--- datastreams/types.go
+++ datastreams/types.go
@@ -15,6 +15,8 @@ type ExpandFunc[T any, U any] func(T) ([]U, error)
 // This function type is used to filter a given input type
 type FilterFunc[T any] func(T) (bool, error)
 
+type KeyFunc[T any, K comparable] func(T) K
+
 type receivers[T any] []<-chan T
 type senders[T any] []chan<- T
 type pipes[T any] []chan T
