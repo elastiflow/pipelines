207db6b85ce365a09d784d87f4c64cc30a31e5ac
Peter Toms <169079833+petertoms1@users.noreply.github.com>
2025-04-25 10:42:28 -0500

    Add Expand function for Pipelines (#15)

--- datastreams/types.go
+++ datastreams/types.go
@@ -7,6 +7,10 @@ type ProcessFunc[T any] func(T) (T, error)
 // This function type is used to transform a given input type to a given output type
 type TransformFunc[T any, U any] func(T) (U, error)
 
+// ExpandFunc is a user defined function type used in a given DataStream stage
+// This function type is used to expand a given input into multiple outputs
+type ExpandFunc[T any, U any] func(T) ([]U, error)
+
 // FilterFunc is a user defined function type used in a given DataStream stage
 // This function type is used to filter a given input type
 type FilterFunc[T any] func(T) (bool, error)
