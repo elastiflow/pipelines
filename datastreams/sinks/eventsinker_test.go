000a9f180e8ad424962e29a6e8bf534197a6030c
Reggie Ross <reggieross07@gmail.com>
2025-06-25 15:31:51 -0500

    feat: adding event sourcer and sinker

---
+++ datastreams/sinks/eventsinker_test.go
@@ -0,0 +1,65 @@
+package sinks
+
+import (
+	"context"
+	"github.com/elastiflow/pipelines/datastreams"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"testing"
+	"time"
+)
+
+type mockEventProducer[T any] struct {
+	mock.Mock
+}
+
+func (m *mockEventProducer[T]) Produce(msg T) {
+	m.Called(msg)
+}
+
+func TestToEventProducer(t *testing.T) {
+	producer := &mockEventProducer[int]{}
+	sinker := ToEventProducer[int](producer)
+	assert.NotNil(t, sinker)
+}
+
+func TestEventProducer_Sink(t *testing.T) {
+	type testCase[T any] struct {
+		name   string
+		sender chan T
+		params Params
+		input  []T
+		want   []T
+	}
+
+	tests := []testCase[int]{
+		{
+			name:   "given valid params, should send data",
+			sender: make(chan int, 128),
+			params: Params{BufferSize: 128},
+			input:  []int{1, 2},
+			want:   []int{1, 2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+			defer cancel()
+			ep := &mockEventProducer[int]{}
+			ep.On("Produce", mock.Anything).Return().Times(len(tt.input))
+			inChan := make(chan int, 128)
+			errChan := make(chan error, 128)
+			sourceDS := datastreams.New[int](ctx, inChan, errChan)
+			go func() {
+				for _, val := range tt.input {
+					inChan <- val
+				}
+				close(inChan)
+			}()
+			sink := ToEventProducer[int](ep, tt.params)
+			err := sink.Sink(ctx, sourceDS)
+			assert.NoError(t, err)
+			ep.AssertNumberOfCalls(t, "Produce", len(tt.input))
+		})
+	}
+}
---
+++ datastreams/sources/eventsourcer.go
@@ -0,0 +1,69 @@
+package sources
+
+import (
+	"context"
+	"github.com/elastiflow/pipelines/datastreams"
+)
+
+type MessageMarker[T any] interface {
+	MarkSuccess(msg T)
+}
+
+type ErrorMarker[T any] interface {
+	MarkError(msg T, err error)
+}
+
+type MessageReader[T any] interface {
+	Messages() <-chan T
+}
+
+type Runner interface {
+	Run(ctx context.Context)
+}
+
+type EventConsumer[T any] interface {
+	MessageMarker[T]
+	MessageReader[T]
+	ErrorMarker[T]
+	Runner
+}
+
+type KafkaSourcer[T any] struct {
+	consumer   EventConsumer[T]
+	streamSize int
+}
+
+func NewEventSourcer[T any](
+	streamSize int,
+	eventConsumer EventConsumer[T],
+) *KafkaSourcer[T] {
+	return &KafkaSourcer[T]{
+		streamSize: streamSize,
+		consumer:   eventConsumer,
+	}
+}
+
+func (k *KafkaSourcer[T]) Source(ctx context.Context, errSender chan<- error) datastreams.DataStream[T] {
+	out := make(chan T, k.streamSize)
+	go k.consumer.Run(ctx)
+	go func(ctx context.Context, msgStream <-chan T, outStream chan<- T) {
+		defer close(out)
+		for msg := range k.consumer.Messages() {
+			select {
+			case <-ctx.Done():
+				return
+			default:
+				out <- msg
+			}
+		}
+	}(ctx, k.consumer.Messages(), out)
+	return datastreams.New[T](ctx, out, errSender)
+}
+
+func (k *KafkaSourcer[T]) MarkSuccess(msg T) {
+	k.consumer.MarkSuccess(msg)
+}
+
+func (k *KafkaSourcer[T]) MarkError(msg T, err error) {
+	k.consumer.MarkError(msg, err)
+}
